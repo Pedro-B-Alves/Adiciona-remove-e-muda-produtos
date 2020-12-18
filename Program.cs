6433b130d574a5f1edc6fd62364a3d9bfb9f50ff
Pedro Alves <pedro22carro@gmail.com>
2020-12-17 22:30:04 -0300

    Atualização do menu

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace Adiciona_remove_e_muda_produtos
         {
             bool opcao = true;
             bool opcao3 = true;
+            bool opcao6 = true;
             Carrinho carrinho = new Carrinho();
             do
             {
@@ -19,36 +20,42 @@ namespace Adiciona_remove_e_muda_produtos
                 switch (opcao2)
                 {
                     case "1":
-                        Produto produto1 = new Produto(1, "Mouse Razer 3.5", 300);
-                        Produto produto2 = new Produto(2, "HeadSet Chroma 7.1", 500);
-                        Produto produto3 = new Produto(3, "Mouse Pad XPTO", 120);
-
-                        carrinho.AdicionarProduto(produto1);
-                        carrinho.AdicionarProduto(produto2);
-                        carrinho.AdicionarProduto(produto3);
-                        Console.WriteLine("Mouse Razer 3.5, HeadSet Chroma 7.1 e Mouse Pad XPTO adicionados");
-
-                        Console.WriteLine("Deseja remover algum? s/n");
-                        string opcao4 = Console.ReadLine();
-                        if (opcao4 == "s"){
-                            carrinho.RemoverProduto(produto2); //Aqui eu removo da lista
-                            Console.WriteLine($"Produto 2 {produto2.Nome} foi removido");
-                        }
-                        Console.WriteLine($"Deseja alterar um produto? s/n");
-                        string opcao5 = Console.ReadLine();
-                        if (opcao5 == "s" && opcao4 != "s")
+                        if (opcao6 == true)
                         {
-                            Produto produtoNovo = new Produto();
-                            produtoNovo.Codigo = 20;
-                            produtoNovo.Nome = "Fone com microfone Chroma 7.1";
-                            produtoNovo.Preco = 600;
+                            Produto produto1 = new Produto(1, "Mouse Razer 3.5", 300);
+                            Produto produto2 = new Produto(2, "HeadSet Chroma 7.1", 500);
+                            Produto produto3 = new Produto(3, "Mouse Pad XPTO", 120);
 
-                            carrinho.AlterarItem(2, produtoNovo);
-                            Console.WriteLine($"O produto foi alterado para {produtoNovo.Nome}");
+                            carrinho.AdicionarProduto(produto1);
+                            carrinho.AdicionarProduto(produto2);
+                            carrinho.AdicionarProduto(produto3);
+                            Console.WriteLine("Mouse Razer 3.5, HeadSet Chroma 7.1 e Mouse Pad XPTO adicionados");
+
+                            Console.WriteLine("Deseja remover algum? s/n");
+                            string opcao4 = Console.ReadLine();
+                            if (opcao4 == "s"){
+                                carrinho.RemoverProduto(produto2); //Aqui eu removo da lista
+                                Console.WriteLine($"Produto 2 {produto2.Nome} foi removido");
+                            }
+                            Console.WriteLine($"Deseja alterar um produto? s/n");
+                            string opcao5 = Console.ReadLine();
+                            if (opcao5 == "s" && opcao4 != "s")
+                            {
+                                Produto produtoNovo = new Produto();
+                                produtoNovo.Codigo = 20;
+                                produtoNovo.Nome = "Fone com microfone Chroma 7.1";
+                                produtoNovo.Preco = 600;
+
+                                carrinho.AlterarItem(2, produtoNovo);
+                                Console.WriteLine($"O produto foi alterado para {produtoNovo.Nome}");
+                            }else{
+                                Console.WriteLine("Devido você ter removido um produto não da para alterá-lo");
+                            }
+                            opcao3 = false;
+                            opcao6 = false;
                         }else{
-                            Console.WriteLine("Devido você ter removido um produto não da para alterá-lo");
+                            Console.WriteLine("Você já adicionou produtos");
                         }
-                        opcao3 = false;
                         break;
                     case "2":
                         if (opcao3 == true){
